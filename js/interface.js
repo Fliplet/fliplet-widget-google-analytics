34e3633d70c1802a1d92b01c9428b7c02715b257
Tony Wu <tonytlwu@gmail.com>
2017-02-08 15:07:07 +0000

    Silly typo

--- js/interface.js
+++ js/interface.js
@@ -3,8 +3,8 @@ $('form').submit(function (event) {
 
   Fliplet.Widget.save({
     trackerName: 'custom',
-    webTracker: 'UA+' + $('[name="webTracker"]').val().replace(/\s/gi, '').replace(/^UA[ ]-/i, ''),
-    nativeTracker: 'UA+' + $('[name="nativeTracker"]').val().replace(/\s/gi, '').replace(/^UA-/i, '')
+    webTracker: 'UA-' + $('[name="webTracker"]').val().replace(/\s/gi, '').replace(/^UA-/i, ''),
+    nativeTracker: 'UA-' + $('[name="nativeTracker"]').val().replace(/\s/gi, '').replace(/^UA-/i, '')
   }).then(function () {
     Fliplet.Widget.complete();
   });
